ba0a4e068989c887683af03f2dbb24252ccf65ce
Alexey Zemel <loyxa.zemel@gmail.com>
2025-03-07 22:48:45 +0400

    finish script and layout

--- script.js
+++ script.js
@@ -2,10 +2,34 @@ function whenButtonClicked() {
     let number1 = parseFloat(document.getElementById("number1").value);
     let number2 = parseFloat(document.getElementById("number2").value);
     let operation = document.getElementById("operation").value;
+    let previous_field = document.getElementById("previous");
+    let current_field = document.getElementById("current");
+    let num1_field = document.getElementById("number1");
+    let num2_field = document.getElementById("number2");
     let result;
     if (isNaN(number1) || isNaN(number2)) {
-        result = 0;
-        // smth
+        if (isNaN(number1) && isNaN(number2)) {
+            num1_field.value ="";
+            num2_field.value ="";
+            num1_field.placeholder = "Введите число!";
+            num1_field.style.borderStyle = "solid";
+            num2_field.placeholder = "Введите число!";
+            num2_field.style.borderStyle = "solid";
+        }
+        else if (isNaN(number1)) {
+            num1_field.value ="";
+            num1_field.placeholder = "Введите число!";
+            num1_field.style.borderStyle = "solid";
+        }
+        else if (isNaN(number2)) {
+            num2_field.value ="";
+            num2_field.placeholder = "Введите число!";
+            num2_field.style.borderStyle = "solid";
+        }
+        previous_field.style.display = "none";
+        current_field.style.display = "block";
+        current_field.innerHTML = "Ошибка!";
+        return;
     }
     switch (operation) {
         case '+':
@@ -16,8 +40,13 @@ function whenButtonClicked() {
             break;
         case '/':
             if (Math.abs(number2) < Number.EPSILON) {
-                result = 0;
-                break;
+                previous_field.style.display = "none";
+                current_field.style.display = "block";
+                num2_field.value ="";
+                num2_field.placeholder = "Введите тут не ноль!";
+                num2_field.style.borderStyle = "solid";
+                current_field.innerHTML = "На ноль делить нельзя!";
+                return;
             }
             result = number1 / number2;
             break;
@@ -25,6 +54,11 @@ function whenButtonClicked() {
             result = number1 * number2;
             break;
     }
-    document.getElementById("result").innerHTML = result;
+    num1_field.style.borderStyle = "dashed";
+    num2_field.style.borderStyle = "dashed";
+    previous_field.innerHTML = current_field.innerHTML;
+    previous_field.style.display = "block";
+    current_field.style.display = "block";
+    current_field.innerHTML = `${number1} ${operation} ${number2} = ${result}`;
     console.log(number1, number2, operation);
 }
